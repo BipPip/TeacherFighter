e9e014f33b4b8ce0dbe85306b0bc2fec79adb004
Caleb Hardy <43180087+ClabHardy@users.noreply.github.com>
2020-02-12 09:21:13 -0700

    Fixed vonderehe lariat damaging himself when crouching

--- TeacherFighter/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ TeacherFighter/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -107,13 +107,13 @@ namespace UnityStandardAssets._2D
 
         private void Flip()
         {
-            // Switch the way the player is labelled as facing.
-            m_FacingRight = !m_FacingRight;
+            // // Switch the way the player is labelled as facing.
+            // m_FacingRight = !m_FacingRight;
 
-            // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
-            theScale.x *= -1;
-            transform.localScale = theScale;
+            // // Multiply the player's x local scale by -1.
+            // Vector3 theScale = transform.localScale;
+            // theScale.x *= -1;
+            // transform.localScale = theScale;
         }
     }
 }
