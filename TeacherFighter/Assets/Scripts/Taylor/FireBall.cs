6a42880d5e12e9e9be5c68b27b7450898697db1e
ClabHardy <caleb.hardy@mtchs.org>
2020-02-15 21:19:56 -0700

    setup basic sound

--- TeacherFighter/Assets/Scripts/Taylor/FireBall.cs
+++ TeacherFighter/Assets/Scripts/Taylor/FireBall.cs
@@ -11,23 +11,30 @@ public class FireBall : MonoBehaviour
     public GameObject impactEffect;
     public LayerMask enemyLayers;
 
+    private AudioSource audioData;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioData = gameObject.GetComponent<AudioSource>();
+        // audioData.Play();
     }
 
     void Update(){
         rb.velocity = new Vector2(speed * transform.localScale.x, 0);
+        
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        
 
         if(col.gameObject.tag == "Player"){
             col.gameObject.GetComponent<Damage>().doDamage(5f, 0.5f);
-
+            
         }
+        AudioSource.PlayClipAtPoint(audioData.clip, gameObject.transform.position);
         Destroy(gameObject);
         Instantiate(impactEffect, transform.position, transform.rotation);
     }
