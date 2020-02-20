62a6ecfed5a967046a4c6ce8921ec7d17dcff6a2
Tia Flores <43146386+tiaflores@users.noreply.github.com>
2020-02-20 09:29:56 -0700

    yep

--- TeacherFighter/Assets/Scripts/StartMenuBlink.cs
+++ TeacherFighter/Assets/Scripts/StartMenuBlink.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
- using UnityEngine.UI;
-using UnityEngine;
+using UnityEngine.UIElements;
+
 
 public class StartMenuBlink : MonoBehaviour
 {
     
     // this is the UI.Text or other UI element you want to toggle
-    public MaskableGraphic imageToToggle;
+    public Image imageToToggle;
  
     public float interval = 1f;
     public float startDelay = 0.5f;
