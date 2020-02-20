526717906c8ace6e3fcb6aaf0fba5ad244bdc1fd
Caleb Hardy <43180087+ClabHardy@users.noreply.github.com>
2020-02-20 10:21:43 -0700

    WIP: blocking

--- TeacherFighter/Assets/Scripts/Damage.cs
+++ TeacherFighter/Assets/Scripts/Damage.cs
@@ -14,13 +14,16 @@ public class Damage : MonoBehaviour
     float h2;
     float v;
     float v2;
-     bool p2block;
+    bool p2block;
+
+    private Stamina stamina;
 
     // Start is called before the first frame update
     void Start()
     {
         this.anim = gameObject.GetComponent<Animator>();
         this.playerHealthBar = gameObject.GetComponent<PlatformerCharacter2D>().healthBarObject.GetComponent<SimpleHealthBar>();
+        this.stamina = gameObject.GetComponent<Stamina>();
         
     }
 
@@ -32,17 +35,17 @@ public class Damage : MonoBehaviour
         v = CrossPlatformInputManager.GetAxis("Vertical");
         v2 = CrossPlatformInputManager.GetAxis("Vertical2");
 
-        if (CrossPlatformInputManager.GetButtonDown("Vertical2")) {
+        if (CrossPlatformInputManager.GetButton("Vertical2")) {
             p2block = true;
         } else {
-            
+            p2block = false;
         }
-
-        if (((gameObject.GetComponent<PlatformerCharacter2D>().m_FacingRight && v == 1 && h < 1 && h > -1) || !gameObject.GetComponent<PlatformerCharacter2D>().m_FacingRight && v2 < 0 && CrossPlatformInputManager.GetButtonDown("Vertical2")) && !this.anim.GetCurrentAnimatorStateInfo(0).IsName("Stun")) {
+        
+        if (this.stamina.getStamina() >= 5 && ((gameObject.GetComponent<PlatformerCharacter2D>().m_FacingRight && v == 1 && h < 1 && h > -1) || !gameObject.GetComponent<PlatformerCharacter2D>().m_FacingRight && v2 < 0 && CrossPlatformInputManager.GetButton("Vertical2")) && !this.anim.GetCurrentAnimatorStateInfo(0).IsName("Stun")) {
             anim.SetTrigger("Block");
             // v2 = 0;
         }
-        Debug.Log(v2);
+        Debug.Log(p2block);
 
         if (this.anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") || this.anim.GetCurrentAnimatorStateInfo(0).IsName("Walk") || this.anim.GetCurrentAnimatorStateInfo(0).IsName("Run") || this.anim.GetCurrentAnimatorStateInfo(0).IsName("Crouch") || this.anim.GetCurrentAnimatorStateInfo(0).IsName("CrouchingWalk") 
         || this.anim.GetCurrentAnimatorStateInfo(0).IsName("Jumping") || this.anim.GetCurrentAnimatorStateInfo(0).IsName("Die")) {
@@ -69,8 +72,10 @@ public class Damage : MonoBehaviour
         h2 = CrossPlatformInputManager.GetAxis("Horizontal2");
         v = CrossPlatformInputManager.GetAxis("Vertical");
         v2 = CrossPlatformInputManager.GetAxis("Vertical2");
-        if((gameObject.GetComponent<PlatformerCharacter2D>().m_FacingRight && (h < 0 || v < 0) || !gameObject.GetComponent<PlatformerCharacter2D>().m_FacingRight && (h2 > 0 || v2 < 0)) && !this.anim.GetCurrentAnimatorStateInfo(0).IsName("Stun")) {
+        if(this.stamina.getStamina() >= 5 && (gameObject.GetComponent<PlatformerCharacter2D>().m_FacingRight && (h < 0 || v > 0) || !gameObject.GetComponent<PlatformerCharacter2D>().m_FacingRight && (h2 > 0 || v2 < 0)) && !this.anim.GetCurrentAnimatorStateInfo(0).IsName("Stun")) {
             anim.SetTrigger("Block");
+            this.stamina.staminaDecrease(5f);
+            this.stamina.startCountdown(1);
         } else {
 
         this.playerHealthBar.UpdateBar((gameObject.GetComponent<PlatformerCharacter2D>().healthBarObject.GetComponent<SimpleHealthBar>().GetCurrentFraction * 100) - damage, 100);
