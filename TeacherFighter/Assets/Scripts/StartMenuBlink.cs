4b5b090c64e78e4c1f89606e5734707f2a2f8369
Tia Flores <43146386+tiaflores@users.noreply.github.com>
2020-02-21 08:22:00 -0700

    update

--- TeacherFighter/Assets/Scripts/StartMenuBlink.cs
+++ TeacherFighter/Assets/Scripts/StartMenuBlink.cs
@@ -1,44 +1,43 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UIElements;
-
+ using UnityEngine.UI;
+using UnityEngine;
 
 public class StartMenuBlink : MonoBehaviour
 {
-    
+
     // this is the UI.Text or other UI element you want to toggle
-    public Image imageToToggle;
- 
+    public MaskableGraphic imageToToggle;
+
     public float interval = 1f;
     public float startDelay = 0.5f;
     public bool currentState = true;
     public bool defaultState = true;
     bool isBlinking = false;
- 
- 
+
+
     void Start()
     {
-        //imageToToggle.enabled = defaultState;
+        imageToToggle.enabled = defaultState;
         StartBlink();
     }
- 
+
     public void StartBlink()
     {
         // do not invoke the blink twice - needed if you need to start the blink from an external object
         if (isBlinking)
             return;
- 
+
         if (imageToToggle !=null)
         {
             isBlinking = true;
             InvokeRepeating("ToggleState", startDelay, interval);
         }
     }
- 
+
     public void ToggleState()
     {
-        //imageToToggle.enabled = !imageToToggle.enabled;
- 
+        imageToToggle.enabled = !imageToToggle.enabled;
+
     }
 }
