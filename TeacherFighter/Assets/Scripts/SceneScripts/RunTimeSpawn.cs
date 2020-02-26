29723b7ab28e1714e64d3024c4401c47b83207dc
TiaSFlores <tia.simone01@gmail.com>
2020-02-26 09:30:37 -0700

    characterselect

---
+++ TeacherFighter/Assets/Scripts/SceneScripts/RunTimeSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RunTimeSpawn : CharacterSelect
+{
+    // Start is called before the first frame update
+
+    string prefabName;
+
+    void Start()
+    {
+
+        prefabName = getPlayer1();
+
+        //charsel = GameObject.FindObjectOfType<CharacterSelect>();
+        //string charselected = charsel.charName;
+       
+
+
+        //charsel = gm.GetComponent<CharacterSelect>(); 
+        //string charselected = gameObject.GetComponent<CharacterSelect>().charName;
+
+
+
+        Debug.Log();
+        //Debug.Log(charsel.charName);
+
+
+
+
+
+        //char1 = (GameObject)Resources.Load("prefabs/" + charName, typeof(GameObject));
+        //Instantiate(char1, new Vector3(0,0,0), Quaternion.identity);
+    }
+
+}
--- TeacherFighter/Assets/Scripts/SceneScripts/StartMenuBlink.cs
+++ TeacherFighter/Assets/Scripts/SceneScripts/StartMenuBlink.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
- using UnityEngine.UI;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class StartMenuBlink : MonoBehaviour
