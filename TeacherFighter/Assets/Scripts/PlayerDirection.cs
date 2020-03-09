6bb8b554e65d066de14e93e155e5379c4cd045e9
@caleb10 <caleb.hardy@mtchs.org>
2020-03-09 16:20:10 -0600

    Working on player direction

---
+++ TeacherFighter/Assets/Scripts/PlayerDirection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class PlayerDirection : MonoBehaviour
+{
+
+
+    public GameObject player1;
+    public GameObject player2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(player1.transform.position.x);
+        Debug.Log(player1.transform.position.x > player2.transform.position.x);
+        if (player1.transform.position.x > player2.transform.position.x) {
+            player1.GetComponent<PlatformerCharacter2D>().m_FacingRight = false;
+            player1.transform.scale.x = player1.transform.scale.x * -1;
+        }
+
+        
+    }
+}
